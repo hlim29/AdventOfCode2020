7aacafddbdbb32d0b8a5c589f2138ef1248b0657
Henry <hlim29@users.noreply.github.com>
2020-12-11 18:01:03 +1100

    Day eleven optimisation

--- Days/DayEleven.cs
+++ Days/DayEleven.cs
@@ -10,13 +10,13 @@ namespace AdventOfCode2020.Days
         private readonly List<string> _input;
         private readonly Dictionary<(int X, int Y), char> _map;
         private readonly HashSet<(int X, int Y)> _keys;
-        private List<(int X, int Y)> _neighbours;
+        private List<(int X, int Y)> _directions;
 
         public DayEleven()
         {
             _input = ReadFile("dayeleven.txt").ToList();
             _map = new Dictionary<(int X, int Y), char>();
-            _neighbours = new List<(int X, int Y)> { (-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1) };
+            _directions = new List<(int X, int Y)> { (-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1) };
 
             for (int i = 0; i < _input.Count(); i++)
             {
@@ -37,14 +37,15 @@ namespace AdventOfCode2020.Days
 
         private int Solve(int tolerance, bool isPartTwo)
         {
-            var map = Occupy(_map, tolerance, isPartTwo);
-            var occupiedCount = CountOccupied(map);
+            var map = new Dictionary<(int X, int Y), char>(_map);
+            var occupyCounts = new List<int> { CountOccupied(map) };
 
-            while (occupiedCount != CountOccupied(Occupy(map, tolerance, isPartTwo)))
+            while (occupyCounts.Count() < 3 || occupyCounts[^1] != occupyCounts[^2])
             {
+                occupyCounts.Add(CountOccupied(map));
                 map = Occupy(map, tolerance, isPartTwo);
-                occupiedCount = CountOccupied(map);
             }
+
             return CountOccupied(map);
         }
 
@@ -56,7 +57,7 @@ namespace AdventOfCode2020.Days
             {
                 var neighbours = isPartTwo 
                     ? GetVisibleNeighbours(item, map)
-                    : _neighbours.Select(x => (x.X + item.X, x.Y + item.Y)).Where(x => map.TryGetValue((x.Item1, x.Item2), out var value)).Select(x => map[x]).ToList();
+                    : _directions.Select(x => (x.X + item.X, x.Y + item.Y)).Where(x => map.TryGetValue((x.Item1, x.Item2), out var value)).Select(x => map[x]).ToList();
 
                 var occupiedNeighbours = neighbours.Where(x => x == '#').ToList();
                 
@@ -86,7 +87,7 @@ namespace AdventOfCode2020.Days
         {
             var neighbours = new List<char>();
 
-            foreach (var direction in _neighbours)
+            foreach (var direction in _directions)
             {
                 var multiplier = 1;
                 var square = (X: direction.X * multiplier + currentSquare.X, Y: direction.Y * multiplier + currentSquare.Y);
