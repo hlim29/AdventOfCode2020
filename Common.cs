0974e200139637d2f643084ead44e426f4f0024e
Henry <hlim29@users.noreply.github.com>
2020-12-11 18:49:31 +1100

    Updated Square class

--- Common.cs
+++ Common.cs
@@ -13,16 +13,36 @@ namespace AdventOfCode2020
             return File.ReadAllText(@$"Inputs/{filename}");
         }
 
-        public class Coord
+        public class Square
         {
-            public Coord(int x, int y)
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Square(int x, int y)
             {
                 X = x;
                 Y = y;
             }
 
-            public int X { get; set; }
-            public int Y { get; set; }
+            public static Square operator +(Square a, Square b) => new Square(a.X + b.X, a.Y + b.Y);
+            public static Square operator -(Square a, Square b) => new Square(a.X - b.X, a.Y - b.Y);
+            public static Square operator *(Square a, Square b) => new Square(a.X * b.X, a.Y * b.Y);
+            public static bool operator ==(Square a, Square b) => a.X == b.X && a.Y == b.Y;
+            public static bool operator !=(Square a, Square b) => a.X != b.X || a.Y != b.Y;
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null || GetType() != obj.GetType())
+                    return false;
+                
+                var other = (Square)obj;
+                return other.X == X && other.Y == Y;
+            }
+
+            public override string ToString()
+            {
+                return $"{X}, {Y}";
+            }
         }
     }
 }
